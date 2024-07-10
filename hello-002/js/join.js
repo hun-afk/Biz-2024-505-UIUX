fe9a7092e7d5966f44448bc163c36fe3feee6e1f
hun-afk <e_ester@naver.com>
2024-07-10 16:15:00 +0900

    observer

--- hello-002/js/join.js
+++ hello-002/js/join.js
@@ -1,46 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const join_body = document.querySelector("section.join.body")
-    const join_form = join_body.querySelector("form")
-    const input_username = join_body.querySelector("input[name='username']");
-    const input_password = join_body.querySelector("input[name='password']");
-    const input_re_password = join_body.querySelector("input[name='re_password']");
-    const input_name = join_body.querySelector("input[name='name']");
-    const input_gel = join_body.querySelector("input[name='tel']");
-    const btn_join = join_body.querySelector("input[name='join']");
+  const join_body = document.querySelector("section.join.body");
+  const join_form = join_body.querySelector("form");
+  const input_username = join_body.querySelector(
+    "input[name='username']"
+  );
+  const input_password = join_body.querySelector(
+    "input[name='password']"
+  );
+  const input_re_password = join_body.querySelector(
+    "input[name='re_password']"
+  );
+  const input_name = join_body.querySelector("input[name='name']");
+  const input_tel = join_body.querySelector("input[name='tel']");
+  const btn_join = join_body.querySelector("input.join");
 
-    // 회원가입 버튼(btn_join)이 클릭되었을 때 실행 할 함수
-    // 이 함수 내에서 각 데이터의 유효성 검사를 실행한다
-    const onValidation = () => {
-        const value_username = input_username.value;
-        const value_password = input_password.value;
-        const value_re_password = input_re_password.value;
-    };
-    /*
-        join(회원가입) 버튼을 클릭 하였을 때
-            1. username, password, re_password가 입력되었는지 검사하기
-            2. password와 re_password가 서로 일치하는지 검사하기
-    */
-   if(!value_username) {
-        alert("아이디와 비밀번호를 입력해주세요");
-        return false;
-   } 
-   const emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-   if(!emailRule.test(value_username)) {
+  // 회원가입 버튼(btn_join)이 클릭되었을때 실행할 함수
+  // 이 함수내에서 각 데이터의 유효성 검사를 실행한다
+  const onValidation = () => {
+    const value_username = input_username.value;
+    const value_password = input_password.value;
+    const value_re_password = input_re_password.value;
 
-   }
-   if (!value_password) {
-        alert("입력하신 비밀번호가 일치하지 않습니다");
-        input_re_password.select();
-        return false;
-   }
-   const passwordRexp = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
-   if(!passwordRexp.test(value_password)) {
-     alert("비밀번호는 영문, 숫자, 특수문자 포함 8~15자리 규칙으로 입력하세요");
-     input_password.select();
-     return false;
-   }
-   if(!value_re_password) {
-     alert("입력하신 비밀번호가 일치하지 않습니다");
-     return false;
-   }
+    if (!value_username) {
+      alert("USER NAME 은 반드시 입력하세요");
+      input_username.select();
+      return false;
+    }
+
+    // username 정규식
+    const userNameRexp = /^(?=.*[a-z0-9])[a-z0-9]{3,16}$/;
+    if (!userNameRexp.test(value_username)) {
+      alert(
+        "USER NAME 3 ~ 16자리 이내의 영문, 숫자 조합으로 입력하세요"
+      );
+      input_username.select();
+      return false;
+    }
+
+    if (!value_password) {
+      alert("비밀번호는 반드시 입력하세요");
+      input_password.select();
+      return false;
+    }
+    const passwordRexp =
+      /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+    if (!passwordRexp.test(value_password)) {
+      alert(
+        "비밀번호는 영문, 숫자, 특수문자 포함 8 ~ 15자리 규칙으로 입력하세요"
+      );
+      input_password.select();
+      return false;
+    }
+
+    if (!value_re_password) {
+      alert("비밀번호 확인은 반드시 입력하세요");
+      input_re_password.select();
+      return false;
+    }
+
+    // 비밀번호와 비밀번호 확인 다를경우
+    if (value_password !== value_re_password) {
+      alert(
+        "비밀번호와 비밀번호 확인 값이 서로 다릅니다 확인해 주세요"
+      );
+      input_re_password.select();
+      return false;
+    }
+    join_form.submit();
+  };
+  /*
+  join(회원가입) 버튼을 클릭했을때
+  1. username, password, re_password 가 입력되었는지 검사하기
+  2. password 와 re_password 가 일치하는지 검사하기
+  */
+  btn_join.addEventListener("click", onValidation);
 });
--- hello-002/js/login.js
+++ hello-002/js/login.js
@@ -1,38 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const login_body = document.querySelector("section.login.body");
-    const btn_login = login_body.querySelector("input.log");
-    const form = login_body.querySelector("form");
-    const btn_join = login_body.querySelector("section.login.body");
-    const input_username = login_body.querySelector("input[name='usesrname']");
-    const input_password = login_body.querySelector("input[name='password']");
+  const login_body = document.querySelector("section.login.body");
+  const form = login_body.querySelector("form");
+  const btn_login = login_body.querySelector("input.login");
+  const btn_join = login_body.querySelector("button.join");
+  const input_username = login_body.querySelector(
+    "input[name='username']"
+  );
+  const input_password = login_body.querySelector(
+    "input[name='password']"
+  );
 
-    /*
-        로그인을 실행할 때 user id와 password가 입력되었는지 확인하고
-        입력하지않았다면 정확히 입력하도록 유도하는 JS코드
-    */
-    const onValidation = () => {
-        const value_username = input_username.value;
-        const value_password = input_password.value;
-    //    alert(`${value_username}`, `${value_password}`);
+  /*
+  로그인을 실행할때 USER ID 와 PASSWORD 가 입력되었는지 확인하여 
+  입력되지 않았으면 정확히 입력하도록 유도하는 JS 코드
+  */
+  const onValidation = () => {
+    const value_username = input_username.value;
+    const value_password = input_password.value;
+    // alert(`${value_username}, ${value_password}`);
 
-    // 사용자 id가 입력이 안됐으면
-        if(value_username === "") {
-            // 안내 메세지 출력
-            alert("사용자 아이디는 반드시 입력해야 합니다");
-            
-            //username input box에 focus를 주어 사용자가 또 마우스를
-            //사용하는걸 줄여준다
-            input_username.select();
-            // event handler를 중단
-            return false;
-        }
-        if(value_password === "") {
-            alert("비밀번호를 입력해주세요");
-            input_password.select();
-            return false;
-        }
-        // 유효성 검사가 끝났으니 서버로 데이터 전송
-        form.submit();
-    };
-    btn_login.addEventListener("click", onValidation);
+    // 사용자 ID 가 입력이 안됬으면
+    if (value_username === "") {
+      // 안내 메시지를 보여주고
+      alert("사용자 ID는 반드시 입력해야 합니다");
+
+      // username input box 에 focus() 주어 사용자 또다시 마우스를
+      // 클릭하는 것을 줄여준다
+      input_username.select();
+      // event handler 코드 실행을 중단하라
+      return false;
+    }
+    if (!value_password) {
+      alert("비밀번호를 입력해 주세요");
+      input_password.select();
+      return false;
+    }
+    // 유효성 검사가 끝났으니 서버로 데이터를 전송하라
+    form.submit();
+  };
+  btn_login.addEventListener("click", onValidation);
 });
